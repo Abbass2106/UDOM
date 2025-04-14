213c53912538c8702df68fb88439d665979b8506
Abbass2106 <samsonabbass@gmail.com>
2025-04-14 16:33:01 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/<repository-name>/', // Replace <repository-name> with your repository name
   plugins: [react()],
+  base:'/YUDOM',
 })
