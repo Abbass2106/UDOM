271f2f6de275502cd971f526c1d4e5b3415115fa
Abbass2106 <samsonabbass@gmail.com>
2025-04-14 17:05:10 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/<repository-name>/', // Replace <repository-name> with your repository name
+export default defineConfig({ 
   plugins: [react()],
   base:'/UDOM',
 })
